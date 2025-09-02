d31b707990a24e1147d7a6cb27084dde247fc34b
liucaho <liuchao@innovatech.net.cn>
2025-09-02 18:33:47 +0800

    no message

---
+++ UniPluginDemo/components/Demo4Childe/Demo4Childe.vue
@@ -0,0 +1,23 @@
+<template>
+	<view>
+		4444444
+	</view>
+</template>
+
+<script setup>
+import {ref} from "vue"
+const name = ref("张三")
+import{onLoad,onReady} from "@dcloudio/uni-app"
+	onLoad(()=>{
+		name.value = "里斯"
+		console.log("onLoad 函数")
+	})
+	onReady(()=>{
+		
+	})
+	
+</script>
+
+<style lang="scss">
+
+</style>
